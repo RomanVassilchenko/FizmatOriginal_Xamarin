93cf26d52261e1ed2658bcd0ca6e9f14476c64a1
Roman Vassilchenko <36629811+RomanVassilchenko@users.noreply.github.com>
2019-08-07 13:10:32 +0600

    - add helppage - change direction to helppage - add source (TODO change source)

---
+++ FizmatOriginal/FizmatOriginal/Views/HelpPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FizmatOriginal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelpPage : ContentPage
+    {
+        public HelpPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            try
+            {
+                activity_indicator_Web.IsRunning = true;
+                webViewHelp.Source = "https://astana.fizmat.kz/";
+            }
+            catch
+            { 
+            }
+            activity_indicator_Web.IsRunning = false;
+        }
+    }
+}
