e79d2b99661c82b62e15b813ad33c93d1e3d331b
Roman Vassilchenko <roman.vassilchenko.work@outlook.com>
2019-08-11 20:42:24 +0600

    - Add newsPage - Fix bugs - add Navigation.PushAsync

---
+++ FizmatOriginal/FizmatOriginal/Models/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizmatOriginal.Models
+{
+    public class News
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public object date { get; set; }
+        public string image { get; set; }
+        public string url { get; set; }
+    }
+}
--- FizmatOriginal/FizmatOriginal/Views/NewsPage.xaml.cs
+++ FizmatOriginal/FizmatOriginal/Views/NewsPage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using FizmatOriginal.Models;
+using Newtonsoft.Json;
+using Plugin.Connectivity;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +18,56 @@ namespace FizmatOriginal.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewsPage : ContentPage
     {
+        public int Count = 0;
+        public short Counter = 0;
+        public int SlidePosition = 0;
+        int heightRowsList = 90;
+        ObservableCollection<News> trends = new ObservableCollection<News>();
+        private string Url = "https://script.google.com/macros/s/AKfycbyi9dWCzKzI1vR5u3f05KtN6rHTutTd1QoTE-4eSyLDT6XdCTQ/exec";
+        private HttpClient _client = new HttpClient();
+        WebView webView;
+
         public NewsPage()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            OnGetList();
+            myList.ItemSelected += myList_ItemSelectedAsync;
+        }
+
+        private async void myList_ItemSelectedAsync(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedCategory = e.SelectedItem as News;
+            string selectedItem = "";
+            if (selectedCategory != null)
+                selectedItem = selectedCategory.url;
+            await Navigation.PushAsync(new WebPage(selectedItem));
+        }
+
+        protected async void OnGetList()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                try
+                {
+                    activity_indicator.IsRunning = true;
+                    var content = await _client.GetStringAsync(Url);
+                    var tr = JsonConvert.DeserializeObject<List<News>>(content);
+                    trends = new ObservableCollection<News>(tr);
+                    myList.ItemsSource = trends;
+                    int i = trends.Count;
+                    if (i > 0)
+                    {
+                        activity_indicator.IsRunning = false;
+                    }
+                    i = (trends.Count * heightRowsList);
+                    activity_indicator.HeightRequest = i;
+                }
+                catch (Exception ey)
+                {
+                    Debug.WriteLine("" + ey);
+                }
+            }
         }
     }
 }
