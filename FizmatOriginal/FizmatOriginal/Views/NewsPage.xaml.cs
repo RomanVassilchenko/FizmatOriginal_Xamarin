4bdbf241b71e1c1a3a25c56ae6944916ef487e65
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2019-08-25 10:28:49 +0600

    0.5.1 - fix crash application

--- FizmatOriginal/FizmatOriginal/Views/NewsPage.xaml.cs
+++ FizmatOriginal/FizmatOriginal/Views/NewsPage.xaml.cs
@@ -106,34 +106,27 @@ namespace FizmatOriginal.Views
                             ndate[9].ToString();
 
 
-                    string[] dates = ndate.Split(new char[] { '.' });
-                    if (dates[1] == "1" || dates[1] == "01") dates[1] = "Янв";
-                    if (dates[1] == "2" || dates[1] == "02") dates[1] = "Фев";
-                    if (dates[1] == "3" || dates[1] == "03") dates[1] = "Мар";
-                    if (dates[1] == "4" || dates[1] == "04") dates[1] = "Апр";
-                    if (dates[1] == "5" || dates[1] == "05") dates[1] = "Май";
-                    if (dates[1] == "6" || dates[1] == "06") dates[1] = "Июн";
-                    if (dates[1] == "7" || dates[1] == "07") dates[1] = "Июл";
-                    if (dates[1] == "8" || dates[1] == "08") dates[1] = "Авг";
-                    if (dates[1] == "9" || dates[1] == "09") dates[1] = "Сен";
-                    if (dates[1] == "10") dates[1] = "Окт";
-                    if (dates[1] == "11") dates[1] = "Ноя";
-                    if (dates[1] == "12") dates[1] = "Дек";
-                    ndate = dates[0] + " " + dates[1] + " " + dates[2];
-                }
-                if (ndate.Length == 9)
-                {
-                    //01 Апр 19
-                    ndate = ndate[0].ToString() +
-                            ndate[1].ToString() +
-                            ndate[2].ToString() +
-                            ndate[3].ToString() +
-                            ndate[4].ToString() +
-                            ndate[5].ToString() +
-                            ndate[6].ToString() +
-                            "20" +
-                            ndate[7].ToString() +
-                            ndate[8].ToString();
+                    string[] dates = ndate.Split(new char[] { '/' });
+                    try
+                    {
+                        if (dates[0] == "1" || dates[0] == "01") dates[0] = "Янв";
+                        if (dates[0] == "2" || dates[0] == "02") dates[0] = "Фев";
+                        if (dates[0] == "3" || dates[0] == "03") dates[0] = "Мар";
+                        if (dates[0] == "4" || dates[0] == "04") dates[0] = "Апр";
+                        if (dates[0] == "5" || dates[0] == "05") dates[0] = "Май";
+                        if (dates[0] == "6" || dates[0] == "06") dates[0] = "Июн";
+                        if (dates[0] == "7" || dates[0] == "07") dates[0] = "Июл";
+                        if (dates[0] == "8" || dates[0] == "08") dates[0] = "Авг";
+                        if (dates[0] == "9" || dates[0] == "09") dates[0] = "Сен";
+                        if (dates[0] == "10") dates[0] = "Окт";
+                        if (dates[0] == "11") dates[0] = "Ноя";
+                        if (dates[0] == "12") dates[0] = "Дек";
+                        ndate = dates[1] + " " + dates[0] + " " + dates[2].Substring(0, 5);
+                    }
+                    catch
+                    {
+
+                    }
                 }
 
                 jsonfinal.Add(new News
@@ -145,61 +138,14 @@ namespace FizmatOriginal.Views
                     url = n.url
                 });
             }
-            /*
-             foreach (News s in json)
+            switch (jsonfinal.Count)
             {
-                string datadate;
-                if ((s.date.ToString()).Length > 9)
-                {
-                    datadate = s.date.ToString().Substring(0, 9);
-                    string[] dates = datadate.Split(new char[] { '/' });
-                    if (dates[0] == "1") dates[0] = "Янв";
-                    if (dates[0] == "2") dates[0] = "Фев";
-                    if (dates[0] == "3") dates[0] = "Мар";
-                    if (dates[0] == "4") dates[0] = "Апр";
-                    if (dates[0] == "5") dates[0] = "Май";
-                    if (dates[0] == "6") dates[0] = "Июн";
-                    if (dates[0] == "7") dates[0] = "Июл";
-                    if (dates[0] == "8") dates[0] = "Авг";
-                    if (dates[0] == "9") dates[0] = "Сен";
-                    if (dates[0] == "10") dates[0] = "Окт";
-                    if (dates[0] == "11") dates[0] = "Ноя";
-                    if (dates[0] == "12") dates[0] = "Дек";
-                    datadate = dates[1] + " " + dates[0] + " " + dates[2];
-                }
-                else
-                {
-                    datadate = s.date.ToString();
-                }
-                string ntitle, ndesc;
-                ntitle = s.title;
-                ndesc = s.description;
-
-                // REGEX 
-                var pattern = @"&#...;";
-                var rgx = new Regex(pattern);
-                ntitle = rgx.Replace(ntitle, "");
-                ndesc = rgx.Replace(ndesc, "");
-
-
-                jsonfinal.Add(new News
-                {
-                    title = ntitle,
-                    date = datadate,
-                    description = ndesc,
-                    image = s.image,
-                    url = s.url
-                });
-            }
-             */
-            if (jsonfinal.Count == 0)
-            {
-                myList.BackgroundColor = Color.FromHex("#012647");
-
-            }
-            else
-            {
-                myList.BackgroundColor = Color.Black;
+                case 0:
+                    myList.BackgroundColor = Color.FromHex("#012647");
+                    break;
+                default:
+                    myList.BackgroundColor = Color.Black;
+                    break;
             }
             myList.ItemsSource = jsonfinal;
         }
