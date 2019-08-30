96f2ddc84f6ebeca62e97d5e729fd5d4b69392f7
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2019-08-30 19:36:26 +0600

    0.9.1 - Add Login Page - Add Register Page - Add Diary icon - Add Help icon - Add More icon - Change news, schedule and settings icon

---
+++ FizmatOriginal/FizmatOriginal/Views/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FizmatOriginal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_login_Clicked(object sender, EventArgs e)
+        {
+            if (entry_email.Text.Contains("@astana.fizmat.kz"))
+            {
+                
+            }
+            else
+            {
+                DisplayAlert("Ошибка", "Используйте школьную почту", "Ок");
+                entry_email.Text = "";
+                entry_password.Text = "";
+            }
+        }
+
+        private async void Btn_register_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
